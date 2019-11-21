04b324f76e632ee3f8d03a8205f9d0218c38ed83
rafaelespino <rafaelespino200@gmail.com>
2019-11-21 09:14:10 -0600

    Added functionality to move files based on category within a directory

--- organizer.py
+++ organizer.py
@@ -1,10 +1,19 @@
 import os
+import shutil
 
 #gets the current directory which will be organized
 path = os.getcwd()
 
 #All of the folders to be created so that files can be organized in each
-folder_categories = {"Music", "Videos", "Executables and Installers", "Documents", "Other", "Pictures"}
+folder_categories = {"Music and Audio", "Videos", "Executables and Installers", "Documents", "Pictures", "Compressed Files"}
+
+#file extensions for each category
+audio_extensions = {".mp3", ".wav", ".ogg", ".flac", ".wma", ".aac"}
+video_extensions = {".mp4", ".mkv", ".mov", ".wav", ".avi", ".wmv", ".flv"}
+executable_extensions = {".exe", ".dmg", ".pkg", ".msi", ".deb"}
+picture_extensions = {".png", ".jpg", ".gif", ".tiff", ".psd", ".raw", ".HEIC"}
+compressed_extensions = {".zip", ".7z", ".tar", ".rar"}
+document_extensions = {".pdf", ".pptx", ".xlsx", ".doc", ".docx", ".xls", ".ppt", ".pptx"}
 
 #Creates all of the folders for each category if they do not already exist
 for folder in folder_categories:
@@ -17,3 +26,33 @@ for folder in folder_categories:
             print("Creation of the %s directory has failed" % folder)
     else:
         print("%s directory successfully created" % folder)
+
+#moves all files to their respective folder
+for file in os.listdir(path):
+    hasBeenMoved = False
+    for extension in audio_extensions:
+        if(file.endswith(extension)):
+            shutil.move(file, 'Music and Audio/%s' % file)
+
+    for extension in video_extensions:
+        if(file.endswith(extension)):
+            shutil.move(file, 'Videos/%s' % file)
+
+    for extension in executable_extensions:
+        if(file.endswith(extension)):
+            shutil.move(file, 'Executables and Installers/%s' % file)
+
+    for extension in document_extensions:
+        if(file.endswith(extension)):
+            shutil.move(file, 'Documents/%s' % file)
+
+    for extension in picture_extensions:
+        if(file.endswith(extension)):
+            shutil.move(file, 'Pictures/%s' % file)
+
+    for extension in compressed_extensions:
+        if(file.endswith(extension)):
+            shutil.move(file, 'Compressed Files/%s' % file)
+
+
+    
