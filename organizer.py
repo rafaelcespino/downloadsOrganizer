0b317a62841ab361354166ccfb13bf1871571a16
rafaelespino <rafaelespino200@gmail.com>
2019-11-22 17:59:15 -0600

    Added functionality to ask the user which directory to organize

--- organizer.py
+++ organizer.py
@@ -1,8 +1,14 @@
 import os
 import shutil
 
-#gets the current directory which will be organized
-path = os.getcwd()
+#prompts the user to enter the directory to be organized
+try:
+    pathString = input("Please enter the directory that you would like to organize")
+    path = os.path.normpath(pathString)
+    os.chdir(path)
+except FileNotFoundError:
+    print("Error: That directory does not exist")
+
 
 #All of the folders to be created so that files can be organized in each
 folder_categories = {"Music and Audio", "Videos", "Executables and Installers", "Documents", "Pictures", "Compressed Files", 'Others'}
@@ -11,7 +17,7 @@ folder_categories = {"Music and Audio", "Videos", "Executables and Installers", 
 audio_extensions = {".mp3", ".wav", ".ogg", ".flac", ".wma", ".aac"}
 video_extensions = {".mp4", ".mkv", ".mov", ".wav", ".avi", ".wmv", ".flv"}
 executable_extensions = {".exe", ".dmg", ".pkg", ".msi", ".deb"}
-picture_extensions = {".png", ".jpg", ".gif", ".tiff", ".psd", ".raw", ".HEIC"}
+picture_extensions = {".png", ".JPG", ".gif", ".tiff", ".psd", ".raw", ".HEIC"}
 compressed_extensions = {".zip", ".7z", ".tar", ".rar"}
 document_extensions = {".pdf", ".pptx", ".xlsx", ".doc", ".docx", ".xls", ".ppt", ".pptx"}
 
@@ -59,5 +65,7 @@ def moveFiles():
         if(os.path.isfile(file) and file != 'organizer.py'):
             shutil.move(file, 'Others/%s' % file)
 
+
 createFolders()
 moveFiles()
+
